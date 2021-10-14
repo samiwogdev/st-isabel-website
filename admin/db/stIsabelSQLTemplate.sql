f551ea872948cfb5d389ec2acaae009d59cad1ca
Inyang Elemi <83021187+elemiinyang@users.noreply.github.com>
2021-10-14 19:56:44 +0100

    DBUpdateTable

--- admin/db/stIsabelSQLTemplate.sql
+++ admin/db/stIsabelSQLTemplate.sql
@@ -16,3 +16,35 @@ CREATE TABLE IF NOT EXISTS `slider` (
 -- `user_type` int(1) comment '1 => user, 2 => admin 3 => superAdmin'
 );
 
+/*Enrolment*/
+CREATE TABLE IF NOT EXISTS `enrolment` (
+ `id` int(11) primary key auto_increment NOT NULL,
+ `bg_image` varchar(250) NOT NULL,
+`title` varchar(250) NOT NULL,
+`description` varchar(250) NOT NULL,
+`enrol_title` varchar(250) NOT NULL,
+`enrol_desc` varchar(250) NOT NULL
+);
+
+/*QUALITIES*/
+CREATE TABLE IF NOT EXISTS `school_qualities` (
+ `id` int(11) primary key auto_increment NOT NULL,
+`qualities` varchar(250) NOT NULL
+);
+
+/*SCHOOL SONG*/
+CREATE TABLE IF NOT EXISTS `school_song` (
+ `id` int(11) primary key auto_increment NOT NULL,
+`title` varchar(250) NOT NULL,
+`description` varchar(250) NOT NULL
+);
+
+/*SCHOOL SONG*/
+CREATE TABLE IF NOT EXISTS `slider_ext` (
+ `id` int(11) primary key auto_increment NOT NULL,
+`title` varchar(250) NOT NULL,
+`description` varchar(250) NOT NULL
+);
+
+
+
